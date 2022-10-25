4e0a6f0a288d2b61133218c38b503c8ddaa2ffb0
EOreshkova <katsiaryna.areshkava@smddev.com>
2022-10-25 20:49:48 +0300

    third_step

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -3,9 +3,10 @@ import argparse
 
 def gendiff_parser():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
+    parser.add_argument('first_file', help='tests/fixtures/file1.json')
+    parser.add_argument('second_file', help='tests/fixtures/file2.json')
     parser.add_argument('-f', '--format',
                          default='FORMAT',
                          help='set format of output')
-    parser.print_help()
+    args = parser.parse_args()
+    return args
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,8 +1,11 @@
 from gendiff.cli import gendiff_parser
+from gendiff.file_diff import generate_diff
 
 
 def main():
-    gendiff_parser()
+    args = gendiff_parser()
+    diff = generate_diff(args.first_file, args.second_file)
+    print(diff)
 
 
 if __name__ == 'main':
