1fb5a30f1cfada8cd01cfd15dac5bd98bc9fa5de
EOreshkova <katsiaryna.areshkava@smddev.com>
2022-10-28 21:22:53 +0300

    code climate fix

--- gendiff/file_diff.py
+++ gendiff/file_diff.py
@@ -3,46 +3,34 @@ import json
 
 def generate_diff(first_file_path, second_file_path):
     with open(first_file_path, 'r') as f1:
-        unsorted_f1 = json.loads(f1.read())
-        sorted_f1 = json.dumps(unsorted_f1)
-        sorted_f1 = replace_char(sorted_f1)
-        file1 = eval(sorted_f1)
+        file1 = json.loads(f1.read())
     with open(second_file_path, 'r') as f2:
-        unsorted_f2 = json.loads(f2.read())
-        sorted_f2 = json.dumps(unsorted_f2)
-        sorted_f2 = replace_char(sorted_f2)
-        file2 = eval(sorted_f2)
+        file2 = json.loads(f2.read())
 
+    sorted_keys = sorted(
+        list(set(file1.keys()).union(set(file2.keys())))
+    )
     result_dict = {}
-    for item in file1:
-        if item not in file2:
-            internal_dict = {'- ' + item: file1.get(item)}
-            result_dict.update(internal_dict)
 
-    for item in file2:
-        if item not in file1:
-            internal_dict = {'+ ' + item: file2.get(item)}
-            result_dict.update(internal_dict)
+    for i in sorted_keys:
+        if i in file1 and i not in file2:
+            result_dict.update({'- ' + i: file1[i]})
+        if (i in file1 and i in file2) and (file1[i] != file2[i]):
+            result_dict.update({'- ' + i: file1[i]})
+            result_dict.update({'+ ' + i: file2[i]})
+        if (i in file1 and i in file2) and (file1[i] == file2[i]):
+            result_dict.update({'  ' + i: file1[i]})
+        if i in file2 and i not in file1:
+            result_dict.update({'+ ' + i: file2[i]})
 
-    for item1 in file1:
-        if item1 in file2:
-            for item2 in file2:
-                if item1 == item2 and file1[item1] != file2[item2]:
-                    internal_dict1 = {'- ' + item1: file1.get(item1)}
-                    internal_dict2 = {'+ ' + item2: file2.get(item2)}
-                    result_dict.update(internal_dict1)
-                    result_dict.update(internal_dict2)
-                if item1 == item2 and file1[item1] == file2[item2]:
-                    internal_dict = {'  ' + item1: file1.get(item1)}
-                    result_dict.update(internal_dict)
-
-    fin_str = json.dumps(result_dict)
-    fin_str = fin_str.replace("\"", '')
-    return fin_str
+    dict_to_str = json.dumps(result_dict)
+    dict_to_str = replace_char(dict_to_str)
+    dict_to_str = dict_to_str.replace("\"", "")
+    return dict_to_str
 
 
-def replace_char(file):
-    file = file.replace('true', "True")
-    file = file.replace('false', "False")
-    file = file.replace('none', "None")
-    return file
+def replace_char(str_dict):
+    str_dict = str_dict.replace('True', "true")
+    str_dict = str_dict.replace('False', "false")
+    str_dict = str_dict.replace('None', "none")
+    return str_dict
---
+++ tests/test_diff.py
@@ -0,0 +1,21 @@
+import pytest
+from gendiff.file_diff import generate_diff
+
+
+@pytest.fixture
+def path_file1():
+    return 'tests/fixtures/file1.json'
+
+
+@pytest.fixture
+def path_file2():
+    return 'tests/fixtures/file2.json'
+
+
+def test_file_difference():
+    with open('tests/fixtures/etalon.txt', 'r') as file:
+        expected_file = file.readlines()
+    result_file = generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json')
+    assert result_file == str(expected_file)
+
+
